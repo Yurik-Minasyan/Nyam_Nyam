0062a11c00f1cfc61b676fbcda810fdf10a570e6
Yura Minasyan <minas6161@gmail.com>
2023-04-21 21:41:54 +0400

    Cart page

--- app/src/main/java/com/example/nyam_nyam/Cart.java
+++ app/src/main/java/com/example/nyam_nyam/Cart.java
@@ -2,20 +2,15 @@ package com.example.nyam_nyam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Cart extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this. setRequestedOrientation(ActivityInfo. SCREEN_ORIENTATION_PORTRAIT);
-        setContentView(R.layout.activity_main);
-    }
-    public void eng(View view){
-        Intent i = new Intent(MainActivity.this, Main_Menu.class);
-        startActivity(i);
+        setContentView(R.layout.activity_cart);
     }
 }
