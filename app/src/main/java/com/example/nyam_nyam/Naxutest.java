20325f1e8cebc94bb913bcec2bc1dfe7a661a8a3
Yura Minasyan <minas6161@gmail.com>
2023-05-01 23:07:10 +0400

    FireBase connected!

--- app/src/main/java/com/example/nyam_nyam/Naxutest.java
+++ app/src/main/java/com/example/nyam_nyam/Naxutest.java
@@ -1,18 +1,36 @@
 package com.example.nyam_nyam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ktx.Firebase;
+
+import java.util.HashMap;
 
 public class Naxutest extends AppCompatActivity {
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this. setRequestedOrientation(ActivityInfo. SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_naxutest);
+
     }
     public void home2(View view){
         Intent i = new Intent(Naxutest.this, Main_Menu.class);
