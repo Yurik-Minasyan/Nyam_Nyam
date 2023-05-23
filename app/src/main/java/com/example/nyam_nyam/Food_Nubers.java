92253580522a01ab52af3b4adced728fedc1a4a2
Yura Minasyan <minas6161@gmail.com>
2023-05-23 22:34:43 +0400

    update

--- app/src/main/java/com/example/nyam_nyam/Food_Nubers.java
+++ app/src/main/java/com/example/nyam_nyam/Food_Nubers.java
@@ -1,11 +1,28 @@
 package com.example.nyam_nyam;
 
 import android.app.Application;
+import android.view.Choreographer;
 
 public class Food_Nubers extends Application {
     private String Meat;
     private String Cheese;
     private String Marinade;
+    private String Micado;
+    private String Napoleon;
+    private String Donat;
+    private String Arab;
+    private String Cappuccino;
+    private String Latte;
+    private String Cola;
+    private String Fanta;
+    private String Sprite;
+    private String Water;
+    private String French;
+    private String Hot_dog;
+    private String Burger;
+    private String Caprese;
+    private String Caesar;
+    private String Vegetable;
     public String getMeat(){
         return Meat;
     }
@@ -24,5 +41,100 @@ public class Food_Nubers extends Application {
     public void setMarinade(String Marinade){
         this.Marinade = Marinade;
     }
-
+    public String getMicado(){
+        return Micado;
+    }
+    public void setMicado(String Micado){
+        this.Micado=Micado;
+    }
+    public String getNapoleon(){
+        return Napoleon;
+    }
+    public void setNapoleon(String Napoleon){
+        this.Napoleon=Napoleon;
+    }
+    public String getDonat(){
+        return Donat;
+    }
+    public void setDonat(String Donat){
+        this.Donat=Donat;
+    }
+    public String getArab(){
+        return Arab;
+    }
+    public void setArab(String Arab){
+        this.Arab=Arab;
+    }
+    public String getCappuccino(){
+        return Cappuccino;
+    }
+    public void setCappuccino(String Cappuccino){
+        this.Cappuccino=Cappuccino;
+    }
+    public String getLatte(){
+        return Latte;
+    }
+    public void setLatte(String Latte){
+        this.Latte=Latte;
+    }
+    public String getCola(){
+        return Cola;
+    }
+    public void setCola(String Cola){
+        this.Cola=Cola;
+    }
+    public String getFanta(){
+        return Fanta;
+    }
+    public void setFanta(String Fanta){
+        this.Fanta=Fanta;
+    }
+    public String getSprite(){
+        return Sprite;
+    }
+    public void setSprite(String Sprite){
+        this.Sprite=Sprite;
+    }
+    public String getWater(){
+        return Water;
+    }
+    public void setWater(String Water){
+        this.Water=Water;
+    }
+    public String getFrench(){
+        return French;
+    }
+    public void setFrench(String French){
+        this.French=French;
+    }
+    public String getHot_dog(){
+        return Micado;
+    }
+    public void setHot_dog(String Hot_dog){
+        this.Hot_dog=Hot_dog;
+    }
+    public String getBurger(){
+        return Burger;
+    }
+    public void setBurger(String Burger){
+        this.Burger=Burger;
+    }
+    public String getCaprese(){
+        return Caprese;
+    }
+    public void setCaprese(String Caprese){
+        this.Caprese=Caprese;
+    }
+    public String getCaesar(){
+        return Caesar;
+    }
+    public void setCaesar(String Caesar){
+        this.Caesar=Caesar;
+    }
+    public String getVegetable(){
+        return Vegetable;
+    }
+    public void setVegetable(String Vegetable){
+        this.Vegetable=Vegetable;
+    }
 }
